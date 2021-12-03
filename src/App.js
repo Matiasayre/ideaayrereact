509aa8e26e0a5a3b7fde31f01760b0c8309a2efd
matias ayre <matiasayre@gmail.com>
2021-12-02 22:32:57 -0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import logo from './logo.svg';
+import React from "react"
 import './App.css';
-
+import NavBar from './components/navBar';
 function App() {
+  const styles ={
+    color:"red",
+    backgroundColor:"yellow"
+  }
+  const text = "hola que tal "
+
   return (
     <div className="App">
+      <h1>Minimarket</h1>
+       <NavBar/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p style={styles}>hola</p>
+        <p className="color-azul" style={{color:"blue"}}>hola</p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
+          {text}
           Learn React
         </a>
       </header>
---
+++ src/components/navBar.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+function NavBar(){
+    return(
+      <nav class="nav-react">
+          <a href="h" class="link-react">Inicio</a>
+          <a href="h" class="link-react">Tienda</a>
+          <a href="h" class="link-react">Servicios</a>
+          <a href="h" class="link-react">Contacto</a>
+       </nav>
+    )
+}
+
+export default NavBar
