f2b6c96f62d73e6a36354dc10c268a755caef2cb
matias ayre <matiasayre@gmail.com>
2021-12-22 22:40:56 -0300

    8 clase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
-import React from "react"
+import React,{useEffect,useState} from "react"
 import './App.css';
+
+import { BrowserRouter,Routes,Route,NavLink,Link,useParams } from 'react-router-dom';
 import NavBar from './components/navBar';
 import ItemListContainer from './components/ItemListContainer';
 function App() {
@@ -9,12 +11,36 @@ function App() {
     backgroundColor:"yellow"
   }
   const text = "hola que tal "
+   const Home=()=><div><NavLink to={`Landing/${13}`}>Home</NavLink></div>
+   const Landing=()=><div>Landing</div>
+   const List=()=><div><Link to="/home">Home</Link><ItemListContainer greeting="bienvenido" ><p><Link to="/home">Home</Link></p></ItemListContainer></div>
+   const ListParams=()=><div><Link to="/home">Home</Link><ItemListContainer greeting="bienvenido" ><p><Link to="/home">Home</Link></p></ItemListContainer></div>
+  const params= useParams()
+  const [state,setState]= useState()
+  useEffect(()=>{
+    console.log(params)
+    return console.log(params)
+  },[])
+   return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Landing></Landing>}></Route>
+        <Route exact path="/home" element={<Home></Home>}></Route>
+        <Route exact path="/list" element={<List></List>}></Route>
+        <Route exact path="/Landing/:id" element={<ListParams></ListParams>}></Route>
+        <Route exact path="/Landing/" element={<List></List>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+// https://stackblitz.com/edit/react-tlufpe?file=src/App.js
+export default App;
 
-  return (
-    <div className="App">
+
+{/* <div className="App">
        <h1>Minimarket</h1>
        <NavBar/>
-       <ItemListContainer greeting="bienvenido" ></ItemListContainer>
+     
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -33,8 +59,4 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
-  );
-}
-// https://stackblitz.com/edit/react-tlufpe?file=src/App.js
-export default App;
+    </div> */}
